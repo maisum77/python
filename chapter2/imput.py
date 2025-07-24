ac5e7f12ef1e2d63b930782b01130890fad5a8b3
maisum <104243145+maisum77@users.noreply.github.com>
2025-07-24 15:22:50 +0500

    chapter_2_complete

---
+++ chapter2/imput.py
@@ -0,0 +1,8 @@
+a=input("enter a number= ")
+b=input("enter second number= ")
+
+print ("first num= ", a)
+
+
+print ("second num= ", b)
+print("sum= ",int(a)+int(b))
---
+++ chapter2/types
@@ -0,0 +1,3 @@
+a=12
+t=type(a)
+print(t)
