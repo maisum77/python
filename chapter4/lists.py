788ce9cdca5c9cbb59afff52846b8888fa7ed9a5
maisum <104243145+maisum77@users.noreply.github.com>
2025-07-24 19:53:02 +0500

    chapter5

---
+++ chapter4/lists.py
@@ -0,0 +1,7 @@
+list=[0,1,2,3,4]
+list[0]=input("enter fruit=")
+list[1]=input("enter fruit=")
+list[2]=input("enter fruit=")
+list[3]=input("enter fruit=")
+list[4]=input("enter fruit=")
+print(list)
---
+++ chapter4/practice.py
@@ -0,0 +1,9 @@
+list=[0,0,0,0,0,0]
+list[0]=input("enter marks= ")
+list[1]=input("enter marks= ")
+list[2]=input("enter marks= ")
+list[3]=input("enter marks= ")
+list[4]=input("enter marks= ")
+list[5]=input("enter marks= ")
+list.sort()
+print(list)
